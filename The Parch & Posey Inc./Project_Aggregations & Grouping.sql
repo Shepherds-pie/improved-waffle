1bdfd5d096a55096b7f1daa607c6ae67da9bf695
Shepherds-pie <aubezhu19@gmail.com>
2025-12-17 10:47:56 -0500

    Update Project_Aggregations & Grouping.sql
    Signed-off-by: Shepherds-pie <aubezhu19@gmail.com>

--- The Parch & Posey Inc./Project_Aggregations & Grouping.sql
+++ The Parch & Posey Inc./Project_Aggregations & Grouping.sql
@@ -3,27 +3,27 @@ Use the SQL environment below to find the solution for each of the following que
 
 1. Find the total amount of poster_qty paper ordered in the orders table.
 
-SELECT SUM(poster_qty) 
+SELECT SUM(poster_qty) AS total_poster_sales
   FROM orders
 
 2. Find the total amount of standard_qty paper ordered in the orders table.
 
-SELECT SUM(standard_qty) 
+SELECT SUM(standard_qty) AS total_standard_sales
   FROM orders
 
 3. Find the total dollar amount of sales using the total_amt_usd in the orders table.
 
-SELECT SUM(total_amt_usd) 
+SELECT SUM(total_amt_usd) AS total_sales_revenue
   FROM orders
 
 4. Find the total amount spent on standard_amt_usd and gloss_amt_usd paper for each order in the orders table. 
 
-SELECT SUM(standard_amt_usd)
-       SUM(gloss_amt_usd)
+SELECT SUM(standard_amt_usd) AS total_standard_revenue,
+       SUM(gloss_amt_usd) AS total_gloss_revenue
   FROM orders
 
 5. Find the standard_amt_usd per unit of standard_qty paper.
-SELECT SUM(standard_amt_usd) / SUM(standard_qty) AS standard_qty paper per unit
+SELECT ROUND(SUM(standard_amt_usd) / SUM(standard_qty), 2) AS standard_price_per_unit
   FROM orders
 
 ----------------------------------------------------------------------------------------------------------
