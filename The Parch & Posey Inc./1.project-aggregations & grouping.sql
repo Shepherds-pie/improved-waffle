1ffa3d2293dff33aecd497ca3f740a54d06c55cf
Shepherds-pie <aubezhu19@gmail.com>
2025-12-19 09:28:14 -0500

    Implement data aggregation queries in SQL
    Added SQL queries for data aggregation including MIN, MAX, AVERAGE, and MEDIAN calculations for orders and web events.
    
    Signed-off-by: Shepherds-pie <aubezhu19@gmail.com>

--- The Parch & Posey Inc./1.project-aggregations & grouping.sql
+++ The Parch & Posey Inc./1.project-aggregations & grouping.sql
@@ -1,3 +1,64 @@
+Data Aggregation - MIN & MAX
+
+1. When was the earliest order ever placed? Just return a date. 
+SELECT MIN(occurred_at) AS earliest_order_date
+  FROM orders
+
+2. Try performing the same query as in question 1 without using an aggregation function.
+SELECT occurred_at
+  FROM orders
+ORDER BY occurred_at
+  LIMIT 1
+
+3. When did the most recent (latest) web_event occur?
+SELECT MAX(occurred_at) AS latest_event_time
+  FROM web_events
+
+4. Try to perform the result of the previous query without using an aggregation function.
+SELECT occurred_at
+  FROM web_events
+ORDER BY occurred_at DESC
+ LIMIT 1  
+
+5. Find the mean (AVERAGE) amount spent per order on each paper type, as well as the mean amount of each paper type purchased per order. 
+   Your final answer should have 6 values - one for each paper type for the average number of sales, as well as the average amount.
+SELECT ROUND(AVG(standard_qty), 2) AS standard_num,
+       AVG(poster_qty) AS poster_num,
+       AVG(gloss_qty) AS gloss_num,
+       AVG(standard_amt_usd) AS standard_amt,
+       AVG(gloss_amt_usd) AS gloss_amt,
+       AVG(poster_amt_usd) AS poster_amt
+  FROM orders
+
+6. what is the MEDIAN total_usd spent on all orders?
+SELECT AVG(total_amt_usd) AS median_value
+# if total number of rows is odd, there is only one middle value,
+# if total number of rows is even, there are two middle values whose AVG is the median value
+  FROM (
+    SELECT total_amt_usd
+    FROM orders
+    ORDER BY total_amt_usd
+    LIMIT 2 - (SELECT COUNT(*) FROM orders) % 2
+    OFFSET (SELECT (COUNT(*) - 1) / 2 FROM orders)
+) AS sub;
+# so, break the steps: 
+Sort the data_ ORDER BY
+Skip rows until reaching the middle_ OFFSET_calculates where the middle starts.
+Take 1 or 2 middle values
+Average them
+Output one number → the median
+
+7. Get the MEDIAN of total spent on all orders. 
+SELECT ROUND(AVG(total), 2) AS median_value
+  FROM (
+       SELECT total
+       FROM orders
+       ORDER BY total
+       LIMIT 2 - (SELECT COUNT(*) FROM orders) % 2
+       OFFSET (SELECT (COUNT(*)-1) / 2 FROM orders)
+      ) AS sub
+---------------------------------------------------------------------------------------------------------
+
 Data Aggregation - SUM
 
 1. Find the total amount of poster_qty paper ordered in the orders table.
